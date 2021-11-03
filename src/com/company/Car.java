28a29b4e948677f8617546410f9863c2d55ec8d7
zeynepsteel <lilyfoster1987@gmail.com>
2021-11-03 19:29:18 -0400

    first commit

---
+++ src/com/company/Car.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Car {
+
+    public String color;
+    public String model;
+    public int doors;
+
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Car c1=new Car();
+	c1.color="gereen";
+	
+    }
+}
