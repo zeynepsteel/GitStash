7757730fae8eb64cb3605d4a90f1b1dc33f02e89
zeynepsteel <lilyfoster1987@gmail.com>
2021-11-03 20:55:05 -0400

    the blue car

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 	Car c1=new Car();
+<<<<<<< Updated upstream
 	c1.color="gereen";
 	c1.doors=3;
 
 	
+=======
+	c1.color="blue";
+	c1.doors=3;
+	c1.model="BMW";
+>>>>>>> Stashed changes
     }
 }
